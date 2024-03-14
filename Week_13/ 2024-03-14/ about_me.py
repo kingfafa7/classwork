116d1325956157785b70c5735c2d3d42ab2d2525
Mustafa Ali <mali22@bostonk12.org>
2024-03-14 10:24:50 -0400

    files

---
+++ Week_13/ 2024-03-14/ about_me.py
@@ -0,0 +1,7 @@
+# My name is Mustafa.
+# The date today is 3/14/2024.
+
+print("I was born in Boston, MA")
+print("I was born in the United States of America")
+print("I am 14 years old")
+print("My favorite Academic class is Strategies and history")
