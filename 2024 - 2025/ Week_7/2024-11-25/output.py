16526a5f893715f39760d2046337ecbea3cfae98
356326 <mali22@bostonk12.org>
2024-12-09 09:44:31 -0500

    HTML

---
+++ 2024 - 2025/ Week_7/2024-11-25/output.py
@@ -0,0 +1,10 @@
+#Mustafa Ali
+#11/25/2024
+
+print("Mustafa")
+print("Hasan")
+print("Ali")
+print("Mustafa","Hasan","Ali")
+print("Mustafa","Hasan""Ali",end=".\n")
+print("Mustafa","Hasan""Ali",sep="#",end="*\n")
+print("Mustafa","Hasan""Ali",sep="#",end=".\n")
