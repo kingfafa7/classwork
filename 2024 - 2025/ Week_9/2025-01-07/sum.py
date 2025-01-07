2e20d9e4317b3872cfd3d47f687e84b12b96462d
356326 <mali22@bostonk12.org>
2025-01-07 09:31:54 -0500

    HTML

---
+++ 2024 - 2025/ Week_9/2025-01-07/sum.py
@@ -0,0 +1,8 @@
+#Mustafa Hasan Ali
+#1/7/2025
+
+num1=int(input('please Enter a number: '))
+num2=int(input('please Enter another number: '))
+
+sum =(num1 + num2)
+print("The sum of", num1, "and", num2, "is", sum)
