77416a2ff293e0717c4b67f038a61e2ad5bf3966
356326 <mali22@bostonk12.org>
2025-01-08 09:30:26 -0500

    HTML

---
+++ 2024 - 2025/ Week_9/2025-01-08/product.py
@@ -0,0 +1,10 @@
+#Mustafa Hasan Ali
+#1/8/2025
+
+num1=int(input("Enter a number: "))
+num2=int(input("Enter another number: "))
+num3=int(input("Enter a third number: "))
+
+product = num1*num2*num3
+print()
+print(num1,"x",num2,"x",num3,"x","=",product)
