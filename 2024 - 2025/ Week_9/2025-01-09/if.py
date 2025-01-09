aa7ae52c79b0a485bee6092e6bceaf6b288f2579
356326 <mali22@bostonk12.org>
2025-01-09 10:39:03 -0500

    HTML

---
+++ 2024 - 2025/ Week_9/2025-01-09/if.py
@@ -0,0 +1,10 @@
+#Mustafa Hasan ALi
+#1/9/2025
+
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+if num1>num2:
+    print("the biggest number is ",num1)
+else:
+    print("the biggest3"
+          "2 number is ",num2)
