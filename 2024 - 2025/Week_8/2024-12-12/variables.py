0e77a354186e06b968473f7d07f68dda8ce44e29
356326 <mali22@bostonk12.org>
2025-01-10 08:01:55 -0500

    HTML

---
+++ 2024 - 2025/Week_8/2024-12-12/variables.py
@@ -0,0 +1,22 @@
+#Mustafa Hasan Ali
+#12/12/2024
+
+#"Input, variables and types"
+
+name=input('Enter your name: ')
+print(name)
+#This is a string
+
+Age=input('Enter your age: ')
+print(Age)
+#This is a string
+
+age =int(age)
+print(type(age))
+#this is an integer
+
+GPA=input('Enter your GPA: ')
+print(GPA)
+#This is a string
+
+print("I am",name,"and I am",Age,"years old","my Gpa is")
