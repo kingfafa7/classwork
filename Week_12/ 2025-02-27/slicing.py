8d597ae96e8d114da4d67b0de7619017b2025efd
356326 <mali22@bostonk12.org>
2025-03-10 08:58:45 -0400

    HTML

---
+++ Week_12/ 2025-02-27/slicing.py
@@ -0,0 +1,11 @@
+#Mustafa Ali
+#2/27/2025
+
+string =  "CPWD is a vocation. Welcome to Programming and Web Development!"
+print(string[0:4])
+print(string[30::])
+print(string[0:4],string[43:46],string[ 31:62])
+print(string[20:30],string[0:4]+string[-1])
+print(string[20:30],string[8:18]+string[-1])
+print(string[20:30],string[3::-1]+string[-1])
+print(string[0:7],string[31:62]+string[18])
