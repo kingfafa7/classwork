65e5e8bab01ad3c30ca2c3592272b75153bd43c1
356326 <mali22@bostonk12.org>
2025-03-13 10:26:41 -0400

    HTML

---
+++  Week_13/2025-03-12/Review.py
@@ -0,0 +1,64 @@
+# Please leave 2 comments below, one with your name, and the other with
+# today's date
+
+#Mustafa ALi
+#3/12/2025
+
+
+# Create a variable called random_input and use it to get some text from the
+# user.
+
+random_input = input("Enter random text")
+
+
+# If you want to find out what type a variable is in Python, what method do
+# you use?
+
+#type
+
+
+
+# Use the appropriate method to display the type of the random_input
+# variable. Put a comment below the code with the output you get from
+# running the code.
+
+print(type(random_input))
+
+
+# Please create a variable called name and get the name from the user and
+# store it in the variable.  The variable should be a string.
+
+name=str(input("Enter your name"))
+
+
+
+# Use Python to output proof that your variable name is a string
+
+print(type(name))
+
+
+
+# Please create a variable called age and get the name from the user and
+# store it in the variable.  The variable should be an integer.
+
+age = int(input("Enter your age"))
+
+
+# Use Python to output proof that your variable age is an integer
+print(type(age))
+
+
+
+# Please create a variable called gpa and get the gpa from the user and
+# store it in the variable.  The variable should be a float.
+gpa = float(input("Enter your gpa"))
+
+
+# Use Python to output proof that your variable gpa is a float
+print(type(gpa))
+
+
+
+# Use Python to output a one line complete sentence with your name, age and
+# GPA.
+print("your name is",name,",","your age is",age,"and your gpa is",gpa)
