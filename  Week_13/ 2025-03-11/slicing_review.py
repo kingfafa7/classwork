53cf1824e8d28ba09f6e14febef7eb839e4c0ad3
356326 <mali22@bostonk12.org>
2025-03-13 09:04:36 -0400

    HTML

---
+++  Week_13/ 2025-03-11/slicing_review.py
@@ -0,0 +1,9 @@
+#Mustafa ALi
+#3/11/2025
+
+
+first_name = input("Enter your first name")
+last_name = input("Enter your last name")
+print("Look what I did to your name!")
+
+print(first_name[::-1], last_name[::-1])
