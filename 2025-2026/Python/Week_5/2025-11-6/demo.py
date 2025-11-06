92379a38a5fedf54c2f5f84275b09772be3ea419
356326 <mali22@bostonk12.org>
2025-11-06 10:53:49 -0500

    olu wa mi o

---
+++ 2025-2026/Python/Week_5/2025-11-6/demo.py
@@ -0,0 +1,29 @@
+def main():
+    # Mustafa Ali
+    # 11-6-2025
+    name = "Mustafa"
+    age = 16
+    print("Mr.O looks like Tung Tung Sahil.")
+    input("What is your favorite brainrot")
+    brainrot = (input("Tung Tung Sahil"))
+    print(name)
+    print(age)
+    num1 = -67
+    num2 = -67
+    num3 = 0
+    num4 = 0
+    num5 = 300
+    num6 = 300
+    num7 = -67
+    num8 = 0
+    num9 = 300
+    num10 = 300
+    print(id(num1))
+    print(id(num3))
+    print(id(num5))
+
+
+
+
+if __name__ == '__main__':
+    main()
