6d886f533eb8613cef387b43fc9ca37421579b87
356326 <mali22@bostonk12.org>
2025-11-18 14:02:27 -0500

    Chineke

---
+++ 2025-2026/Python/Week_6/11-18-2025/function.py
@@ -0,0 +1,25 @@
+def main():
+    multiply_2(1,2)
+    print(multiply_2(1,2))
+    print(Add_2(1,2))
+    print(divide_2(1,2))
+    print(subtract_2(1,2))
+
+def multiply_2(first,second):
+        return first*second
+
+def Add_2(first ,second):
+    return 1+2
+
+def divide_2(first,second):
+    return first/second
+def subtract_2(first,second):
+    return first-second
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
